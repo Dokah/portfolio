bd65d017e59d740e37dabd0d8d820a7288f3daf8
unknown <kukovecd@krtech.co>
2025-04-02 22:28:41 +0200

    Cleanup

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <title>Portfolio</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
