9710ad2ffe72ef7ab0ce678eaf15c7ed9a80f250
unknown <dodo3334@gmail.com>
2025-04-23 23:23:15 +0200

    Ios adjustments

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -13,7 +13,9 @@ export const Footer = () => {
           <img src={linkedinLogoURL} alt="LinkedIn" className="logo" />
         </a>
       </div>
-      <p>&copy; {new Date().getFullYear()} Dominik Kukovec</p>
+      <span style={{ padding: 5 }}>
+        &copy; {new Date().getFullYear()} Dominik Kukovec
+      </span>
     </div>
   );
 };
