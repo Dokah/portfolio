fb183b412e91799a662328e2b94dd1b6fcb950e5
unknown <dodo3334@gmail.com>
2025-05-06 08:18:01 +0200

    Added fonts and additional stack logos and adjusted css

--- app/components/tech/index.tsx
+++ app/components/tech/index.tsx
@@ -2,6 +2,8 @@ import { CustomCarousel } from "../carousel";
 import ReactLogo from "../../assets/react.svg";
 import NodeLogo from "../../assets/node.svg";
 import NextLogo from "../../assets/next.svg";
+import JestLogo from "../../assets/jest.svg";
+import GitlabLogo from "../../assets/GitLab.svg";
 import RemixLogo from "../../assets/remix.svg";
 import PSQLLogo from "../../assets/psql.svg";
 import AWSLogo from "../../assets/aws.svg";
@@ -12,11 +14,14 @@ import GitLogo from "../../assets/git.svg";
 import RedisLogo from "../../assets/redis.svg";
 import TypescriptLogo from "../../assets/typescript.svg";
 import CloudflareLogo from "../../assets/cloudflare.svg";
+import GithubLogo from "../../assets/github.svg";
 import "./index.css";
+import { isMobile } from "~/utility/utils";
 
 const codingStack = [
   { src: TypescriptLogo, name: "Typescript" },
   { src: ReactLogo, name: "ReactJS" },
+  { src: JestLogo, name: "Jest" },
   { src: NodeLogo, name: "NodeJS" },
   { src: NextLogo, name: "NextJS" },
   { src: RemixLogo, name: "RemixJS" },
@@ -30,7 +35,18 @@ const deploymentStack = [
   { src: Pm2Logo, name: "PM2" },
   { src: GitLogo, name: "Git" },
   { src: RedisLogo, name: "Redis" },
+  { src: GitlabLogo, name: "Gitlab" },
   { src: CloudflareLogo, name: "CloudFlare" },
+  { src: GithubLogo, name: "Github" },
+];
+
+const mobileStack = [
+  { src: TypescriptLogo, name: "Typescript" },
+  { src: ReactLogo, name: "ReactJS" },
+  { src: NodeLogo, name: "NodeJS" },
+  { src: AWSLogo, name: "AWS" },
+  { src: LinuxLogo, name: "Linux" },
+  { src: DockerLogo, name: "Docker" },
 ];
 
 export const Tech = () => {
@@ -42,7 +58,7 @@ export const Tech = () => {
             <img
               src={logo.src}
               alt={logo.name}
-              style={{ width: "60%", height: "auto" }}
+              style={{ width: "60%", height: "50%" }}
             />
           </div>
         ))}
@@ -53,11 +69,24 @@ export const Tech = () => {
             <img
               src={logo.src}
               alt={logo.name}
-              style={{ width: "60%", height: "auto" }}
+              style={{ width: "60%", height: "50%" }}
             />
           </div>
         ))}
       </CustomCarousel>
+      {isMobile() && (
+        <CustomCarousel speed={40} pauseOnHover duplicateCount={10} gap={16}>
+          {mobileStack.map((logo, index) => (
+            <div className="hex" key={index}>
+              <img
+                src={logo.src}
+                alt={logo.name}
+                style={{ width: "60%", height: "50%" }}
+              />
+            </div>
+          ))}
+        </CustomCarousel>
+      )}
     </div>
   );
 };
