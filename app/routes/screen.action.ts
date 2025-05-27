7fbaa52279ee2864cdff2f4c6bab9fef7c2008d8
unknown <dodo3334@gmail.com>
2025-05-27 19:32:55 +0200

    Fixed bundling

--- app/routes/screen.action.ts
+++ app/routes/screen.action.ts
@@ -1,4 +1,4 @@
-import { json, type ActionFunctionArgs } from "@remix-run/node";
+import { json, type ActionFunctionArgs } from "@remix-run/server-runtime";
 import { sendEmail } from "../utility/send-email.server";
 
 export async function action({ request }: ActionFunctionArgs) {
