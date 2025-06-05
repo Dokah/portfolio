f00c4fe6ba65d6bee2370ced61cfa981b851c0da
unknown <dodo3334@gmail.com>
2025-06-05 21:59:57 +0200

    Changed logic for getting env variables in production

--- app/routes/screen.action.ts
+++ app/routes/screen.action.ts
@@ -6,17 +6,15 @@ export async function action({ request, context }: ActionFunctionArgs) {
   const name = formData.get("name")?.toString().trim();
   const email = formData.get("email")?.toString().trim();
   const message = formData.get("message")?.toString().trim();
-  
-  console.log(context, "CONTEXT");
-  console.log(context.env, "ENV");
+
   //@ts-ignore
-  const sendgridApiKey = context.env.SENDGRID_API_KEY;
+  const sendgridApiKey = process.env.SENDGRID_API_KEY;
   //@ts-ignore
 
-  const sendgridToEmail = context.env.SENDGRID_TO_EMAIL;
+  const sendgridToEmail = process.env.SENDGRID_TO_EMAIL;
   //@ts-ignore
   
-  const sendgridFromEmail = context.env.SENDGRID_FROM_EMAIL;
+  const sendgridFromEmail = process.env.SENDGRID_FROM_EMAIL;
 
   console.log(sendgridApiKey,sendgridFromEmail,sendgridToEmail, "VARIABLES")
   
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
-
+import {config} from "dotenv";
+config()
 
 export default defineConfig({
   plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
@@ -10,4 +11,9 @@ export default defineConfig({
     external: ["@remix-run/node", "nodemailer"],
     noExternal: [], 
   },
+  define: {
+    "process.env.SENDGRID_API_KEY": JSON.stringify(process.env.SENDGRID_API_KEY),
+    "process.env.SENDGRID_TO_EMAIL": JSON.stringify(process.env.SENDGRID_TO_EMAIL),
+    "process.env.SENDGRID_FROM_EMAIL": JSON.stringify(process.env.SENDGRID_FROM_EMAIL), 
+  }
 });
