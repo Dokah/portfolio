6bb464bf351d54c312a8761eeb53a3f70c8bada8
unknown <dodo3334@gmail.com>
2025-06-12 22:03:52 +0200

    Added missing env

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,6 @@ export default defineConfig({
     "process.env.SENDGRID_API_KEY": JSON.stringify(process.env.SENDGRID_API_KEY),
     "process.env.SENDGRID_TO_EMAIL": JSON.stringify(process.env.SENDGRID_TO_EMAIL),
     "process.env.SENDGRID_FROM_EMAIL": JSON.stringify(process.env.SENDGRID_FROM_EMAIL), 
+    "process.env.RECAPTCHA_SECRET_KEY": JSON.stringify(process.env.RECAPTCHA_SECRET_KEY),
   }
 });
